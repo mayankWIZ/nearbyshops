f1d661ba5bcfd98c46eb4b29f2bd1b1e3f3e17c7
mayankWIZ <101165568+mayankWIZ@users.noreply.github.com>
2023-06-19 17:03:20 +0530

    fix: static
    fix: static

--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -136,7 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 # STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
