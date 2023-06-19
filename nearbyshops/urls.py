c6f1cd8929f87164b2840d11afe735b54f564141
mayankWIZ <101165568+mayankWIZ@users.noreply.github.com>
2023-06-19 17:25:58 +0530

    fix: serve static url
    fix: serve static url

--- nearbyshops/urls.py
+++ nearbyshops/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from shops import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.shop_list)
-]
+] + static(settings.STATIC_URL)
